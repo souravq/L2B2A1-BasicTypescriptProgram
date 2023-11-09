37e7dd26f9af44b43ca0a8d018e7c986a5cdc4b2
souravq <souravbera515@gmail.com>
2023-11-09 23:14:31 +0530

    Final Changes Done

--- build/Typescript_Assignment_1.js
+++ build/Typescript_Assignment_1.js
@@ -1,6 +1,5 @@
 "use strict";
-//Problem 1:
-// Design a TypeScript function that takes a parameter of a union type (e.g., string | number). If it's a string, return its length. If it's a number, return the square of that number.
+// Problem 1
 function findLengthOrSquare(param) {
     if (typeof param === "string") {
         return param.length;
@@ -10,13 +9,11 @@ function findLengthOrSquare(param) {
     }
     return 0;
 }
-console.log(findLengthOrSquare("Sourav")); //6
-console.log(findLengthOrSquare(2)); //4
 function getAddressCity(param) {
     var _a;
     return (_a = param.address) === null || _a === void 0 ? void 0 : _a.city;
 }
-let personObj = {
+const personObj = {
     name: "Sourav",
     age: 25,
     address: {
@@ -24,10 +21,7 @@ let personObj = {
         street: "NH6"
     }
 };
-console.log(getAddressCity(personObj)); //Kolkata
-//===================================
-// Problem 3:
-// Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
+// Problem 3
 class Cat {
     constructor(name, color) {
         this.name = name;
@@ -43,12 +37,7 @@ function isCat(param) {
         return "no, it's not a cat.";
     }
 }
-console.log(isCat(cat)); //yes, it's a cat.
-//===========================
-// Problem 4:
-// You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the total by adding them up.
-// To solve this in TypeScript, you'll look at each thing in the list named mixedData, which looks like this: [1, 'two', 3, 'four', 5]. You'll go through the list, find the numbers, and add them together.
-// Every time you find a number in the list, you'll add it to a total. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions.
+// Problem 4
 const mixedData = [1, 'two', 3, 'four', 5];
 function getTotal(mixedDataParam) {
     let total = 0;
@@ -59,10 +48,7 @@ function getTotal(mixedDataParam) {
     });
     return total;
 }
-console.log(getTotal(mixedData)); // 9
-//=====================
-// Problem 5:
-// Define two interfaces: Car with properties like make, model, and year, and Driver with properties like name and licenseNumber. Create a function that takes two objects of type Car and Driver and returns an object with the combined properties of both types.
+// Problem 5
 class Car {
     constructor(make, model, year) {
         this.make = make;
@@ -81,10 +67,7 @@ const driver = new Driver("Sourav", "WB50XX1136");
 function combineTypes(carParam, driverParam) {
     return Object.assign(Object.assign({}, carParam), driverParam);
 }
-console.log(combineTypes(car, driver)); //{make: 'Tata',model: 'Tata Nano',year: '2012',name: 'Sourav',licenseNumber: 'WB50XX1136'}
-//==========================
-// Problem 6:
-// Write a TypeScript function that takes a parameter of type unknown and uses a type guard to check whether the parameter is an array of numbers. If it is, calculate the sum of the numbers and log it. If it's not, log an error message.
+// Problem 6
 function addArrayNumbers(arrayParam) {
     let total = 0;
     if (Array.isArray(arrayParam)) {
@@ -93,25 +76,13 @@ function addArrayNumbers(arrayParam) {
                 total += data;
             }
             else {
-                return "Array's all elements are not number!!!";
+                console.log("Array's all elements are not number!!!");
             }
         }
     }
-    return total;
+    console.log(total);
 }
-console.log(addArrayNumbers([1, 2, 3, 4, "5"])); //Array's all elements are not number!!!
-//=======================
 // Problem 7:
-// Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
-// Example usage:
-// const numbers: number[] = [1, 2, 3, 4, 5, 2];
-// const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
-// const targetNumber = 2;
-// const targetString = "cherry";
-// const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
-// const indexInStrings = findFirstOccurrence(strings, targetString);
-// console.log(indexInNumbers); //output:  1
-// console.log(indexInStrings); //output: 2
 function findFirstOccurrence(arrayParam, target) {
     for (let i = 0; i < arrayParam.length; i++) {
         if (arrayParam[i] === target) {
@@ -126,8 +97,6 @@ const targetNumber = 2;
 const targetString = "cherry";
 const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
 const indexInStrings = findFirstOccurrence(strings, targetString);
-console.log(indexInNumbers); //output:  1
-console.log(indexInStrings); //output: 2
 function calculateCartCost(arrayParam) {
     let totalCost = 0;
     arrayParam.forEach((product) => {
@@ -152,5 +121,3 @@ const cartArr = [
         quantity: 2
     }
 ];
-console.log(calculateCartCost(cartArr)); //8000
-//==========================================
--- src/Typescript_Assignment_1.ts
+++ src/Typescript_Assignment_1.ts
@@ -1,6 +1,4 @@
-//Problem 1:
-
-// Design a TypeScript function that takes a parameter of a union type (e.g., string | number). If it's a string, return its length. If it's a number, return the square of that number.
+// Problem 1
 
 function findLengthOrSquare(param:string|number):number{
     if(typeof param === "string"){
@@ -11,14 +9,7 @@ function findLengthOrSquare(param:string|number):number{
     return 0;
 }
 
-console.log(findLengthOrSquare("Sourav")); //6
-console.log(findLengthOrSquare(2)); //4
-
-
-//======================
-
-// Create an interface called Person with optional properties address and phone. The address property itself will be another object containing city and street properties. Implement a function named getAddressCity that takes an argument of type Person and returns the city from the address property of the Person object. Use optional chaining to prevent any type errors.
-
+// Problem 2
 
 interface Person{
     name:string;
@@ -34,7 +25,7 @@ function getAddressCity(param:Person){
     return param.address?.city;
 }
 
-let personObj:Person={
+const personObj:Person={
     name:"Sourav",
     age:25,
     address:{
@@ -44,12 +35,7 @@ let personObj:Person={
 
 }
 
-console.log(getAddressCity(personObj)); //Kolkata
-
-//===================================
-// Problem 3:
-
-// Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
+// Problem 3
 
 class Cat{
     name:string;
@@ -70,17 +56,7 @@ function isCat(param:Cat):string{
     }
 }
 
-console.log(isCat(cat)); //yes, it's a cat.
-
-//===========================
-
-// Problem 4:
-
-// You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the total by adding them up.
-
-// To solve this in TypeScript, you'll look at each thing in the list named mixedData, which looks like this: [1, 'two', 3, 'four', 5]. You'll go through the list, find the numbers, and add them together.
-
-// Every time you find a number in the list, you'll add it to a total. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions.
+// Problem 4
 
 const mixedData:(number|string)[] = [1, 'two', 3, 'four', 5];
 
@@ -94,13 +70,7 @@ function getTotal(mixedDataParam:(number|string)[]):number{
     return total;
 }
 
-console.log(getTotal(mixedData)); // 9
-
-//=====================
-
-// Problem 5:
-
-// Define two interfaces: Car with properties like make, model, and year, and Driver with properties like name and licenseNumber. Create a function that takes two objects of type Car and Driver and returns an object with the combined properties of both types.
+// Problem 5
 
 class Car{
     make:string;
@@ -130,54 +100,24 @@ function combineTypes(carParam:Car, driverParam:Driver):(Car|Driver){
     return {...carParam,...driverParam};
 }
 
-console.log(combineTypes(car,driver)); //{make: 'Tata',model: 'Tata Nano',year: '2012',name: 'Sourav',licenseNumber: 'WB50XX1136'}
+// Problem 6
 
-//==========================
-
-// Problem 6:
-
-// Write a TypeScript function that takes a parameter of type unknown and uses a type guard to check whether the parameter is an array of numbers. If it is, calculate the sum of the numbers and log it. If it's not, log an error message.
-
-function addArrayNumbers(arrayParam:unknown):(number|string){
+function addArrayNumbers(arrayParam:unknown){
     let total=0;
     if(Array.isArray(arrayParam)){
         for(let data of arrayParam){
             if(typeof data == "number"){
                 total+=data;
             }else{
-                return "Array's all elements are not number!!!";
+                console.log("Array's all elements are not number!!!");
             }
         }
     }
-    return total;
+    console.log(total);
 }
 
-console.log(addArrayNumbers([1,2,3,4,"5"])); //Array's all elements are not number!!!
-
-
-//=======================
 
 // Problem 7:
-
-// Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
-
-// Example usage:
-
-// const numbers: number[] = [1, 2, 3, 4, 5, 2];
-
-// const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
-
-// const targetNumber = 2;
-
-// const targetString = "cherry";
-
-// const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
-
-// const indexInStrings = findFirstOccurrence(strings, targetString);
-
-// console.log(indexInNumbers); //output:  1
-
-// console.log(indexInStrings); //output: 2
 
 function findFirstOccurrence<S>(arrayParam:Array<S>,target:S):number{
     for(let i=0; i<arrayParam.length; i++){
@@ -200,15 +140,8 @@ const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
 
 const indexInStrings = findFirstOccurrence(strings, targetString);
 
-console.log(indexInNumbers); //output:  1
-
-console.log(indexInStrings); //output: 2
-
-//=====================================
 
 // Problem 8:
-
-// Create a TypeScript program that simulates a simple shopping cart. Define an interface Product with properties like name, price, and quantity. Implement a function that calculates the total cost of the items in the shopping cart. The function should take an array of Product objects as input and return the total cost.
 
 interface Product{
     name:string;
@@ -242,12 +175,6 @@ const cartArr:Product[]=[
     }
 ];
 
-console.log(calculateCartCost(cartArr)); //8000
-
-
-
-
-//==========================================
 
 
 
