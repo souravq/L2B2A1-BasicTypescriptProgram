74b00cfa91882d081985b0fe212d94fe51f93e59
souravq <souravbera515@gmail.com>
2023-11-09 14:27:44 +0530

    2nd program done

--- build/Typescript_Assignment_1.js
+++ build/Typescript_Assignment_1.js
@@ -9,3 +9,17 @@ function Problem1(param) {
 }
 console.log(Problem1("Sourav"));
 console.log(Problem1(2));
+function getAddressCity(param) {
+    var _a;
+    return (_a = param.address) === null || _a === void 0 ? void 0 : _a.city;
+}
+let personObj = {
+    name: "Sourav",
+    age: 25,
+    address: {
+        city: "Kolkata",
+        street: "NH6"
+    }
+};
+console.log(getAddressCity(personObj));
+//===================================
--- src/Typescript_Assignment_1.ts
+++ src/Typescript_Assignment_1.ts
@@ -11,6 +11,40 @@ console.log(Problem1("Sourav"));
 console.log(Problem1(2));
 
 
+//======================
+
+// Create an interface called Person with optional properties address and phone. The address property itself will be another object containing city and street properties. Implement a function named getAddressCity that takes an argument of type Person and returns the city from the address property of the Person object. Use optional chaining to prevent any type errors.
+
+
+interface Person{
+    name:string;
+    age:number;
+    address?:{
+        city: string;
+        street: string;
+    };
+    phone?: number;
+}
+
+function getAddressCity(param:Person){
+    return param.address?.city;
+}
+
+let personObj:Person={
+    name:"Sourav",
+    age:25,
+    address:{
+        city:"Kolkata",
+        street:"NH6"
+    }
+
+}
+
+console.log(getAddressCity(personObj));
+
+//===================================
+
+
 
 
 
