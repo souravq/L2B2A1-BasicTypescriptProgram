81c530c1be5297ff2630953c409ee1c4e5e6bf7a
souravq <souravbera515@gmail.com>
2023-11-09 14:57:00 +0530

    Problem 3 is done

--- build/Typescript_Assignment_1.js
+++ build/Typescript_Assignment_1.js
@@ -23,3 +23,22 @@ let personObj = {
 };
 console.log(getAddressCity(personObj));
 //===================================
+// Problem 3:
+// Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
+class Cat {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+}
+const cat = new Cat("TOM", "Black & White");
+function isCat(param) {
+    if (param instanceof Cat) {
+        return "yes, it's a cat.";
+    }
+    else {
+        return "no, it's not a cat.";
+    }
+}
+console.log(isCat(cat));
+//===========================
--- src/Typescript_Assignment_1.ts
+++ src/Typescript_Assignment_1.ts
@@ -43,6 +43,34 @@ let personObj:Person={
 console.log(getAddressCity(personObj));
 
 //===================================
+// Problem 3:
+
+// Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
+
+class Cat{
+    name:string;
+    color:string;
+    constructor(name:string,color:string){
+        this.name = name;
+        this.color = color;
+    }
+}
+
+const cat = new Cat("TOM", "Black & White");
+
+function isCat(param:Cat){
+    if(param instanceof Cat){
+        return "yes, it's a cat.";
+    }else{
+        return "no, it's not a cat.";
+    }
+}
+
+console.log(isCat(cat));
+
+//===========================
+
+
 
 
 
