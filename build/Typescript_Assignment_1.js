db82400e0a3fc10d2360bc0dc884602b1d3ad300
souravq <souravbera515@gmail.com>
2023-11-09 17:36:04 +0530

    Problem 7 is Done

--- build/Typescript_Assignment_1.js
+++ build/Typescript_Assignment_1.js
@@ -101,3 +101,31 @@ function addArrayNumbers(arrayParam) {
 }
 console.log(addArrayNumbers([1, 2, 3, 4, "5"]));
 //=======================
+// Problem 7:
+// Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
+// Example usage:
+// const numbers: number[] = [1, 2, 3, 4, 5, 2];
+// const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
+// const targetNumber = 2;
+// const targetString = "cherry";
+// const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
+// const indexInStrings = findFirstOccurrence(strings, targetString);
+// console.log(indexInNumbers); //output:  1
+// console.log(indexInStrings); //output: 2
+function findFirstOccurrence(arrayParam, target) {
+    for (let i = 0; i < arrayParam.length; i++) {
+        if (arrayParam[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+const numbers = [1, 2, 3, 4, 5, 2];
+const strings = ["apple", "banana", "cherry", "date", "apple"];
+const targetNumber = 2;
+const targetString = "cherry";
+const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
+const indexInStrings = findFirstOccurrence(strings, targetString);
+console.log(indexInNumbers); //output:  1
+console.log(indexInStrings); //output: 2
+//====================================
